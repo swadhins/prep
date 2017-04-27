a374ca973a744b5f9738ac8acb0e7fc72fdfd3ce
Swadhin Sonowal <swadhin@Swadhins-MacBook-Pro.local>
2017-04-27 10:47:52 +0530

    updating prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 echo "Downloading libraries....."
 curl -O -L  https://github.com/swadhins/prep/raw/master/publibs.tgz 
-echo "Extracting files....""
+echo "Extracting files...."
 tar -zxvf publibs.tgz 
 echo "Cleaning up..."
 rm -f publibs.tgz 
