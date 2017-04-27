ff3683716a9a1a1cceb86a1310270be96829bbfb
Swadhin Sonowal <swadhin@Swadhins-MacBook-Pro.local>
2017-04-27 10:38:58 +0530

    Adding Scripts

---
+++ compress.sh
@@ -0,0 +1,18 @@
+tar zcvf publibs.tgz \
+OpenSSL/ \
+apiclient/ \
+appengine_config.py \
+cloudstorage/ \
+google-apis-client-generator/ \
+googleapiclient/ \
+googlemaps/ \
+httplib2/ \
+oauth2client/ \
+pyfcm/ \
+recaptcha/ \
+requests/ \
+requests_toolbelt/ \
+six.py \
+social/ \
+uritemplate/ 
+
---
+++ prepare.sh
@@ -0,0 +1,7 @@
+echo "Downloading libraries....."
+curl -O -L  https://github.com/swadhins/prep/raw/master/publibs.tgz 
+echo "Extracting files....""
+tar -zxvf publibs.tgz 
+echo "Cleaning up..."
+rm -f publibs.tgz 
+rm -- "$0"
